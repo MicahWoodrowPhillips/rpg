f6ad9e32da6b05cad419d62512ad3bc01714f7f4
wphillips <wphillips@payfone>
2018-11-01 09:38:14 -0600

    Move and pom
    Move BasePFCharacter to more appropriate package, updated POM to use
    JDK8 rather than whatever env it is in.

---
+++ src/main/java/com/lrg/model/character/d20/pathfinder/BasePFCharacter.java
@@ -0,0 +1,8 @@
+package com.lrg.model.character.d20.pathfinder;
+
+import com.lrg.model.character.d20.BaseD20Character;
+
+public abstract class BasePFCharacter extends BaseD20Character
+{
+    
+}
--- src/main/java/com/lrg/model/character/pathfinder/BasePFCharacter.java
+++
@@ -1,8 +0,0 @@
-package com.lrg.model.character.pathfinder;
-
-import com.lrg.model.character.d20.BaseD20Character;
-
-public abstract class BasePFCharacter extends BaseD20Character
-{
-    
-}
